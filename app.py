a4be6e07614903e0af83765408154f3435448ee9
nirupvinay <nirup.vinay@khatabook.care>
2026-02-14 18:14:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -558,7 +558,7 @@ for idx, param in enumerate(template_data["parameters"]):
     st.markdown(
     """
     <div style="
-        height:1px;
+        height:3px;
         background: linear-gradient(
             to right,
             transparent,
