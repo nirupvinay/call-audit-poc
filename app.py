09467b04961fe9446560ff0a4d74ff28bc68d622
nirupvinay <nirup.vinay@khatabook.care>
2026-02-14 18:13:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -195,11 +195,6 @@ if len(template_data["parameters"]) == 0:
 
 for idx, param in enumerate(template_data["parameters"]):
 
-    st.markdown(
-        '<div style="background:#5c2e3e;padding:16px;border-radius:10px;margin-bottom:4px;border:2px solid #8b454e;">',
-        unsafe_allow_html=True
-    )
-
     # TITLE - 1/4 width
     title_col, empty_col = st.columns([3, 9])
     with title_col:
