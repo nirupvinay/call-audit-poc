5c594232a324021df074dd61771b17f23cb53fc2
nirupvinay <nirup.vinay@khatabook.care>
2026-02-15 21:52:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-pwd = st.text_input("Enter password to open app", type="password")
+st.markdown("### 🔐 Khatabook AI Auditor Login")
+pwd = st.text_input("Enter Password", type="password")
 
 if pwd != st.secrets["APP_PASSWORD"]:
     st.stop()
