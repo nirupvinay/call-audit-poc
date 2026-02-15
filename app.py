dfb93db45b4d646a25f605d2df9d2bef7903caf7
nirupvinay <nirup.vinay@khatabook.care>
2026-02-15 21:50:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+import streamlit as st
+
+pwd = st.text_input("Enter password to open app", type="password")
+
+if pwd != st.secrets["APP_PASSWORD"]:
+    st.stop()
+
 from audit_app.ui import run_app
 
 
